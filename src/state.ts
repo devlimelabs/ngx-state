84536e54d01ee5996358e711898cab7355a68c08
Jon Pribesh <john@devlimelabs.com>
2020-04-12 22:56:31 -0400

    feature: Adds get to state class; Preparation for sync persist/getters

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,9 @@
+import { clone } from 'clone';
+
 export class State<T> {
+  get<K extends keyof T>(prop: K): T[K] {
+    return clone(this[prop as string];
+  }
   set<K extends keyof T>(prop: K, value: T[K]): void {
     this[prop as string] = value;
   }
