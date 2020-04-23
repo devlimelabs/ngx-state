54f0e95d85c763e0491607ce1dfdf7b87ccc9327
Jon Pribesh <john@devlimelabs.com>
2020-04-22 20:33:31 -0400

    fix(state): removes get from state in prep for improved implemntation

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,6 @@
 import { clone } from 'clone';
 
 export class State<T> {
-  get<K extends keyof T>(prop: K): T[K] {
-    return clone(this[prop as string];
-  }
   set<K extends keyof T>(prop: K, value: T[K]): void {
     this[prop as string] = value;
   }
