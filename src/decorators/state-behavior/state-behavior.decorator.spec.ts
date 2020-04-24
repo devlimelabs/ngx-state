88d1dfa0a982fb9609bf8298a56a38baed34b257
Jon Pribesh <john@devlimelabs.com>
2020-04-23 21:06:39 -0400

    improvement(statebehavior): fixes StateBehavior assigning props to prototype instead of instance

--- src/decorators/state-behavior/state-behavior.decorator.spec.ts
+++ src/decorators/state-behavior/state-behavior.decorator.spec.ts
@@ -1,149 +1,106 @@
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { TestState } from '../../test/test.state';
-import { StateBehavior } from './state-behavior.decorator';
+import { async, TestBed } from '@angular/core/testing';
 
 describe('@StateBehavior', () => {
   let state: TestState;
 
   beforeEach(() => {
-    state = new TestState();
-  });
+    TestBed.configureTestingModule({
+      providers: [ TestState ]
+    });
 
-  afterEach(() => {
-    state = null;
+    state = TestBed.inject(TestState);
   });
 
   describe('no initial value', () => {
-    beforeEach(() => {
-      StateBehavior()(state, 'testBehavior');
-    });
-
-    it('should create the public, private, and observable properties', done => {
-      expect(state['_testBehavior'] instanceof BehaviorSubject).toBeTruthy();
+    it('should create the observable and static properties', async(() => {
       expect(state.testBehavior$ instanceof Observable).toBeTruthy();
 
       state.testBehavior$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).toEqual(null);
-          done();
+          expect(state.testBehavior).toBeUndefined();
         });
-    });
-
-    it('should emit value set with setter', done => {
-      state['testBehavior'] = 'new value';
-
-      state.testBehavior$
-        .pipe(take(1))
-        .subscribe(value => {
-          expect(value).toEqual('new value');
-          done();
-        });
-    });
+    }));
   });
 
   describe('initial value', () => {
-    beforeEach(() => {
-      StateBehavior('initial value')(state, 'testBehavior');
-    });
+    it('should create the public, private, and observable properties', async(() => {
+      expect(state.testBehaviorDefault$ instanceof Observable).toBeTruthy();
 
-    it('should create the public, private, and observable properties', done => {
-      expect(state['_testBehavior'] instanceof BehaviorSubject).toBeTruthy();
-      expect(state.testBehavior$ instanceof Observable).toBeTruthy();
-
-      state.testBehavior$
+      state.testBehaviorDefault$
         .pipe(take(1))
         .subscribe(value => {
-          expect(value).toEqual('initial value');
-          done();
+          expect(value).toEqual('default');
         });
-    });
+    }));
 
-    it('should emit value set with setter', done => {
+    it('should emit value set with setter', async(() => {
       state.set('testBehavior', 'new value');
+      expect(state.testBehavior).toEqual('new value');
 
       state.testBehavior$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).toEqual('new value');
-          done();
         });
-    });
+    }));
   });
 
   describe('immutability', () => {
-    beforeEach(() => {
-      StateBehavior([])(state, 'testBehaviorArr');
-      StateBehavior({})(state, 'testBehaviorObj');
-    });
-
-    it('should set a copy of an array', done => {
+    it('should set a copy of an array', async(() => {
       const setArray = [1, 2, 3];
 
       state.set('testBehaviorArr', setArray);
-
-      expect(state['_testBehaviorArr'].value).not.toBe(setArray);
-      expect(state['_testBehaviorArr'].value).toEqual(setArray);
 
       state.testBehaviorArr$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).toEqual(setArray);
           expect(value).not.toBe(setArray);
-          done();
         });
-    });
+    }));
 
-    it('should get a copy of an array', done => {
+    it('should get a copy of an array', async(() => {
       const setArray = [1, 2, 3];
 
       state.set('testBehaviorArr', setArray);
-
-      expect(state['_testBehaviorArr'].value).not.toBe(setArray);
-      expect(state['_testBehaviorArr'].value).toEqual(setArray);
 
       state.testBehaviorArr$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).toEqual(setArray);
           expect(value).not.toBe(setArray);
-          done();
         });
-    });
+    }));
 
-    it('should set a copy of an object', done => {
+    it('should set a copy of an object', async(() => {
       const setObject = { one: 1, two: 2, three: 3 };
 
       state.set('testBehaviorObj', setObject);
-
-      expect(state['_testBehaviorObj'].value).not.toBe(setObject);
-      expect(state['_testBehaviorObj'].value).toEqual(setObject);
 
       state.testBehaviorObj$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).not.toBe(setObject);
           expect(value).toEqual(setObject);
-          done();
         });
-    });
+    }));
 
-    it('should get a copy of an object', done => {
+    it('should get a copy of an object', async(() => {
       const setObject = { one: 1, two: 2, three: 3 };
 
       state.set('testBehaviorObj', setObject);
-
-      expect(state['_testBehaviorObj'].value).not.toBe(setObject);
-      expect(state['_testBehaviorObj'].value).toEqual(setObject);
 
       state.testBehaviorObj$
         .pipe(take(1))
         .subscribe(value => {
           expect(value).not.toBe(setObject);
           expect(value).toEqual(setObject);
-          done();
         });
-    });
+    }));
   });
 });
--- src/decorators/state-behavior/state-behavior.decorator.ts
+++ src/decorators/state-behavior/state-behavior.decorator.ts
@@ -1,22 +1,37 @@
 import { clone } from '../../clone';
 import { BehaviorSubject } from 'rxjs';
+import { State } from '../../state';
 
-export function StateBehavior<T, P>(initialValue: P = null) {
-  return function(target: T, key: string) {
-    const behaviorSubjectKey = `_${key}`;
+export function StateBehavior<T extends State<T>, P>(initialValue: P = null) {
+  return function(target: T, key: string): any {
+    const behaviorSubjectKey = Symbol();
     const observableKey = `${key}$`;
 
-    Object.defineProperty(target, behaviorSubjectKey, {
-        value: new BehaviorSubject<P>(initialValue)
-    });
+    function getBehaviorSubject(instance: T): BehaviorSubject<P> {
+      if (!instance[behaviorSubjectKey]) {
+        instance['values'][behaviorSubjectKey] = initialValue;
+        instance[behaviorSubjectKey] = new BehaviorSubject<P>(initialValue);
+      }
+
+      return instance[behaviorSubjectKey];
+    }
 
     Object.defineProperty(target, observableKey, {
       enumerable: true,
-      value: target[behaviorSubjectKey].asObservable()
+      get() {
+        return getBehaviorSubject(this).asObservable();
+      },
     });
 
-    Object.defineProperty(target, key, {
-        set: (value: P): void => target[behaviorSubjectKey].next(clone<P>(value))
-    });
+    return {
+      enumerable: true,
+      get() {
+        return clone(this.values[key]);
+      },
+      set(value: P) {
+        this.values[key] = value;
+        getBehaviorSubject(this).next(clone(value));
+      },
+    };
   };
 }
