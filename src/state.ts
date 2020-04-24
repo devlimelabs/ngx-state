46b4b942b359eea1ef8d2dd563dcfd5d19445f14
Jon Pribesh <john@devlimelabs.com>
2020-04-23 20:20:05 -0400

    feat(state): adds get method to state for synchronous value retrieval

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,17 @@
 import { clone } from 'clone';
 
 export class State<T> {
+  private values: Partial<T>;
+
+  get<K extends keyof T>(prop: K): T[K] {
+    return clone(this.values[prop]);
+  }
+
   set<K extends keyof T>(prop: K, value: T[K]): void {
     this[prop as string] = value;
+  }
+
+  constructor() {
+    this.values = {}
   }
 }
